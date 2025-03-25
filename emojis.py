1273a0b9c9527853debf68847f714b14102f44a4
Jonas J. Solsvik <267431@usn.no>
2025-03-25 18:48:53 +0100

    Add skier() function

--- emojis.py
+++ emojis.py
@@ -5,7 +5,11 @@ def smiley():
 def rocket():
 	return "🚀"
 
+def skier():
+	return "⛷️"
+
 # Test
 if __name__ == "__main__":
 	print("smiley() -> ", smiley())
 	print("rocket() -> ", rocket())
+	print("skier() -> ", skier())
